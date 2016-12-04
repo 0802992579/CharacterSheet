c0917742a519b684ec6867fd90e78899df1e4c25
0802992579 <olafura@live.com>
2016-12-04 20:29:48 +0000

    2 útgáfa
    Problems with GitHub. I solved it by enter this through the web

---
+++ CharacterSheet/CharacterSheet/Form1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CharacterSheet
+{
+    public partial class Form1 : Form
+    {
+        Gagnagrunnur gagnagrunnur = new Gagnagrunnur();
+        Char charact = new Char();
+        int CharacterID = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void checkBox23_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] data;
+            this.tabControl1.SelectTab(2);
+            dataGridCharacters.RowTemplate.Height = 50;
+            List<string> list = new List<string>();
+            list = gagnagrunnur.AllCharacters();
+
+            
+            
+            
+            for (int i = 0; i < list.Count; i++)
+            {
+                data = Convert.ToString(list[i]).Split(':');
+                DataGridViewRow row1 = (DataGridViewRow)dataGridCharacters.Rows[i].Clone();
+                row1.Cells[0].Value = data[0];
+                row1.Cells[1].Value = data[1];
+                row1.Cells[2].Value = data[2];
+                row1.Cells[3].Value = data[3];
+                row1.Cells[4].Value = data[4];
+                dataGridCharacters.Rows.Add(row1);
+            }
+
+         }
+
+        private void setjaIForm()
+        {
+            txtBxAlignment.Text = charact.charAlign;
+            txtBxCharName.Text = charact.charName;
+            txtBxClass.Text = charact.charClass;
+            txtBxRace.Text = charact.charRace;
+            txtBxLvl.Text = Convert.ToString(charact.level);
+            txtBxXp.Text = Convert.ToString(charact.exp);
+            txtBxAC.Text = Convert.ToString(charact.armor);
+            txtBxHp.Text = Convert.ToString(charact.hp);
+            txtBxHpMax.Text = Convert.ToString(charact.maxhp);
+            txtBxStatSTR.Text = Convert.ToString(charact.STR);
+            txtBxStatSTRMOD.Text = Convert.ToString(charact.STR_MOD);
+            txtBxStatDEX.Text = Convert.ToString(charact.DEX);
+            txtBxStatDEXMOD.Text = Convert.ToString(charact.DEX_MOD);
+            txtBxStatCON.Text = Convert.ToString(charact.CON);
+            txtBxStatCONMOD.Text = Convert.ToString(charact.CON_MOD);
+            txtBxStatINT.Text = Convert.ToString(charact.INT);
+            txtBxStatINTMOD.Text = Convert.ToString(charact.INT_MOD);
+            txtBxStatWIS.Text = Convert.ToString(charact.WIS);
+            txtBxStatWISMOD.Text = Convert.ToString(charact.WIS_MOD);
+            txtBxStatCHR.Text = Convert.ToString(charact.CHA);
+            txtBxStatCHAMOD.Text = Convert.ToString(charact.CHA_MOD);
+
+            cbAcrobatic.Checked = charact.acrobatics;
+            cbArcana.Checked = charact.arcana;
+            cbAthletics.Checked = charact.athletics;
+            cbCHA_ST.Checked = charact.chaSave;
+            cbCON_ST.Checked = charact.conSave;
+            cbDeception.Checked = charact.deception;
+            cbDEX_ST.Checked = charact.dexSave;
+            cbHistory.Checked = charact.history;
+            cbIntimidation.Checked = charact.intimidation;
+            cbINT_ST.Checked = charact.intSave;
+            cbInvestigation.Checked = charact.investigation;
+            cbMedicine.Checked = charact.medicine;
+            cbNature.Checked = charact.nature;
+            cbPerception.Checked = charact.perception;
+            cbPersuasion.Checked = charact.persuasion;
+            cbPreformance.Checked = charact.preformance;
+            cbReligion.Checked = charact.religion;
+            cbSleightOfHand.Checked = charact.sleight;
+            cbStealth.Checked = charact.stealth;
+            cbSTR_ST.Checked = charact.strSave;
+            cbSurvival.Checked = charact.survival;
+            cbWIS_ST.Checked = charact.wisSave;
+            cbAnimalHandling.Checked = charact.animalhandling;
+            cbInsight.Checked = charact.insight;
+
+        }
+
+        private void dataGridCharacters_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+           
+         if  (e.RowIndex >= 0)
+            {
+                //ná id úr row sem er smellt á 
+                try
+                {
+                   CharacterID = Convert.ToInt32(dataGridCharacters.Rows[e.RowIndex].Cells[0].Value.ToString());
+                }
+                catch (Exception ex)
+                {
+                    return;
+                }
+                // dataGridCharacters.
+                //ná í character og setja inn
+                string[] gogn = new string[4];
+                gogn = gagnagrunnur.FinnaCharacter(CharacterID);
+                charact.charName = gogn[0];
+                charact.charClass = gogn[1];
+                charact.charRace = gogn[2];
+                charact.charAlign = gogn[3];
+                int[] gogn1 = new int[18];
+                gogn1 = gagnagrunnur.FinnaStats(CharacterID);
+                statICharacter(gogn1); //setja StatICharacter
+                bool[] gogn2 = new bool[25];
+                gogn2 = gagnagrunnur.FinnaAbilities(1);
+                abilitiesICharacter(gogn2); //setja AbilitiesICharacter
+                setjaIForm();
+                btnSave.Enabled = true;
+                this.tabControl1.SelectTab(0);
+            }
+
+        }
+
+        public void statICharacter(int[] gogn)
+        {
+            charact.level = gogn[1];
+            charact.exp = gogn[2];
+            charact.armor = gogn[3];
+            charact.maxhp = gogn[4];
+            charact.hp = gogn[5];
+            charact.STR = gogn[6];
+            charact.DEX = gogn[7];
+            charact.CON = gogn[8];
+            charact.INT = gogn[9];
+            charact.WIS = gogn[10];
+            charact.CHA = gogn[11];
+            charact.STR_MOD = gogn[12];
+            charact.DEX_MOD = gogn[13];
+            charact.CON_MOD = gogn[14];
+            charact.INT_MOD = gogn[15];
+            charact.WIS_MOD = gogn[16];
+            charact.CHA_MOD = gogn[17];
+        }
+
+        public int[] faStatCharacter()
+        {
+            int[] gogn = new int[18];
+            gogn[0] = CharacterID;
+            gogn[1] = charact.level;
+            gogn[2] = charact.exp;
+            gogn[3] = charact.armor;
+            gogn[4] = charact.maxhp;
+            gogn[5] = charact.hp;
+            gogn[6] = charact.STR;
+            gogn[7] = charact.DEX;
+            gogn[8] = charact.CON;
+            gogn[9] = charact.INT;
+            gogn[10] = charact.WIS;
+            gogn[11] = charact.CHA;
+            gogn[12] = charact.STR_MOD;
+            gogn[13] = charact.DEX_MOD;
+            gogn[14] = charact.CON_MOD;
+            gogn[15] = charact.INT_MOD;
+            gogn[16] = charact.WIS_MOD;
+            gogn[17] = charact.CHA_MOD;
+            return gogn;
+        }
+
+        public void abilitiesICharacter(bool[] gogn)
+        {
+            charact.strSave = gogn[1];
+            charact.athletics = gogn[2];
+            charact.dexSave = gogn[3];
+            charact.acrobatics = gogn[4];
+            charact.sleight = gogn[5];
+            charact.stealth = gogn[6];
+            charact.conSave = gogn[7];
+            charact.intSave = gogn[8];
+            charact.arcana = gogn[9];
+            charact.history = gogn[10];
+            charact.investigation = gogn[11];
+            charact.nature = gogn[12];
+            charact.religion = gogn[13];
+            charact.wisSave = gogn[14];
+            charact.animalhandling = gogn[15];
+            charact.insight = gogn[16];
+            charact.medicine = gogn[17];
+            charact.perception = gogn[18];
+            charact.survival = gogn[19];
+            charact.chaSave = gogn[20];
+            charact.deception = gogn[21];
+            charact.intimidation = gogn[22];
+            charact.preformance = gogn[23];
+            charact.persuasion = gogn[24];
+
+        }
+
+        public bool[] faAbiltiesCharacter()
+        {
+            bool[] gogn = new bool[25];
+            gogn[0] = false;//dummy
+            gogn[1] = charact.strSave;
+             gogn[2] = charact.athletics;
+             gogn[3] = charact.dexSave;
+             gogn[4] = charact.acrobatics;
+             gogn[5] = charact.sleight;
+             gogn[6] = charact.stealth;
+             gogn[7] = charact.conSave;
+             gogn[8] = charact.intSave;
+             gogn[9] = charact.arcana;
+             gogn[10] = charact.history;
+             gogn[11] = charact.investigation;
+             gogn[12] = charact.nature;
+             gogn[13] = charact.religion;
+             gogn[14] = charact.wisSave;
+             gogn[15] = charact.animalhandling;
+             gogn[16] = charact.insight;
+             gogn[17] = charact.medicine;
+             gogn[18] = charact.perception;
+             gogn[19] = charact.survival;
+             gogn[20] = charact.chaSave;
+             gogn[21] = charact.deception;
+             gogn[22] = charact.intimidation;
+             gogn[23] = charact.preformance;
+             gogn[24] = charact.persuasion;
+            return gogn;
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            gagnagrunnur.UppfaeraCharacter(CharacterID, faStatCharacter(), faAbiltiesCharacter());
+        }
+    }
+
+}
---
+++ CharacterSheet/CharacterSheet/Gagnagrunnur.cs
@@ -0,0 +1,346 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace CharacterSheet
+{
+    class Gagnagrunnur
+    {
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+        string tengistrengur = null;
+        string fyrirspurn = null;
+
+        MySqlConnection sqltenging;
+        MySqlCommand nySQLskipun;
+        MySqlDataReader sqllesari = null;
+
+        public void TengingVidGagnagrunn()
+        {
+            server = "10.200.10.24";
+            database = "0802992579_lokaverkefni";
+            uid = "0802992579";
+            password = "mypassword";
+
+            tengistrengur = "server=" + server + ";userid=" + uid + ";password=" + password + ";database=" + database;
+            sqltenging = new MySqlConnection(tengistrengur);
+
+        }
+
+        private bool OpenConnection()
+        {
+            try
+            {
+                sqltenging.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                throw ex;
+            }
+
+        }
+
+        private bool CloseConnection()
+        {
+            try
+            {
+                sqltenging.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                throw ex;
+            }
+
+        }
+
+        public List<string> AllCharacters()
+        {
+            List<string> faerslur = new List<string>();
+            string lina = null;
+            if (OpenConnection() == true)
+              {
+                  fyrirspurn = "SELECT characters.name, class.class, stats.level, race.race FROM characters " +
+                            "INNER JOIN class ON characters.ClassID=Class.ClassID " +
+                              "INNER JOIN race ON characters.RaceID=Race.RaceID" +
+                              "INNER JOIN stats ON characters.CharacterID= stats.CharacterID" ;
+                  nySQLskipun = new MySqlCommand(fyrirspurn, sqltenging);
+                  sqllesari = nySQLskipun.ExecuteReader();
+
+                  while (sqllesari.Read())
+                  {
+                      for (int i = 0; i < sqllesari.FieldCount; i++)
+                      {
+                          lina += (sqllesari.GetValue(i).ToString()) + ':';
+                      }
+                      faerslur.Add(lina);
+                      lina = null;
+                  }
+                  sqllesari.Close();
+                  CloseConnection();
+              }
+            /*
+            lina = "1:Jon the Great:Barbarian:2:Human";
+            faerslur.Add(lina);*/
+            
+            return faerslur;
+        }
+
+        public List<string> Spells()
+        {
+            List<string> faerslur = new List<string>();
+            string lina = null;
+            if (OpenConnection() == true)
+            {
+                fyrirspurn = "SELECT characters.name, class.class, stats.level, race.race FROM characters " +
+                            "INNER JOIN class ON characters.CharacterID=Class.ClassID " +
+                            "INNER JOIN race ON characters.CharacterID=Race.RaceID" +
+                            "INNER JOIN stats ON characters.CharacterID= Stats.CharacterID;";
+                nySQLskipun = new MySqlCommand(fyrirspurn, sqltenging);
+                sqllesari = nySQLskipun.ExecuteReader();
+
+                while (sqllesari.Read())
+                {
+                    for (int i = 0; i < sqllesari.FieldCount; i++)
+                    {
+                        lina += (sqllesari.GetValue(i).ToString()) + ':';
+                    }
+                    faerslur.Add(lina);
+                    lina = null;
+                }
+                sqllesari.Close();
+                CloseConnection();
+            }
+            /*
+            lina = "Burning Hands:As you hold your hands spread, a thin sheet of fla...:1";
+            faerslur.Add(lina);
+            lina = "Healing Word:A creature of your choice that you can see within ...:0";
+            faerslur.Add(lina);
+            lina = "Bless:(a sprinkling of holy water) You bless up to 3 cre...:0";
+            faerslur.Add(lina);
+            lina = "Cure Wounds:A creature you touch regains a number of hit point...:0";
+            faerslur.Add(lina);
+            lina = "Fog Cloud:You create a 20-foot-radius sphere of fog centered...:0";
+            faerslur.Add(lina);
+            */
+            return faerslur;
+        }
+
+
+        public string[] FinnaCharacter(int CharacterID)
+        {
+            string[] gogn = new string[4];
+           if (OpenConnection() == true)
+            {
+                //query-in sem á að keyrast (má líka fyrir ofan if setninguna
+                fyrirspurn = "SELECT characters.name, class.class, race.race, alignment.alignment FROM characters " +
+                            "INNER JOIN class ON characters.ClassID=Class.ClassID " +
+                            "INNER JOIN race ON characters.RaceID=Race.RaceID" +
+                            "INNER JOIN stats ON characters.AlignmentID= Alignment.AlignmentID" +
+                            "WHERE CharacterID = '" + CharacterID + "'";
+                //create command and assign the the query(fyrirspurn) and connection(tengingu)
+                nySQLskipun = new MySqlCommand(fyrirspurn, sqltenging);
+                sqllesari = nySQLskipun.ExecuteReader();
+                while (sqllesari.Read())
+                {
+                    for (int i = 0; i < 4; i++)
+                    {
+                        gogn[i] = sqllesari.GetValue(i).ToString();
+                    }
+                }
+                sqllesari.Close();
+                CloseConnection();
+            }
+            /*
+            gogn[0] = "Jonh the great";
+            gogn[1] = "Bard";
+            gogn[2] = "Dwarf";
+            gogn[3] = "Lawfull Neutral";
+            */
+            return gogn;
+        }
+
+
+
+        public int[] FinnaStats(int CharacterID)
+        {
+            int[] gogn = new int[18];
+            if (OpenConnection() == true)
+            {
+                //query-in sem á að keyrast (má líka fyrir ofan if setninguna
+                fyrirspurn = "SELECT * FROM stats WHERE CharacterID ='" + CharacterID + "'";
+                //create command and assign the the query(fyrirspurn) and connection(tengingu)
+                nySQLskipun = new MySqlCommand(fyrirspurn, sqltenging);
+                sqllesari = nySQLskipun.ExecuteReader();
+                while (sqllesari.Read())
+                {
+                    for (int i = 0; i < 18; i++)
+                    {
+                        if (i != 0)
+                        { gogn[i] = Int32.Parse(sqllesari.GetValue(i).ToString()); }
+                    }
+                }
+                sqllesari.Close();
+                CloseConnection();
+            }
+            /*
+            gogn[0] = 1;
+            gogn[1] = 2;
+            gogn[2] = 200;
+            gogn[3] = 8;
+            gogn[4] = 150;
+            gogn[5] = 100;
+            gogn[6] = 14;
+            gogn[7] = 13;
+            gogn[8] = 11;
+            gogn[9] = 12;
+            gogn[10] = 9;
+            gogn[11] = 16;
+            gogn[12] = 2;
+            gogn[13] = 0;
+            gogn[14] = -1;
+            gogn[15] = -3;
+            gogn[16] = 5;
+            gogn[17] = 0;
+            */
+
+            return gogn;
+        }
+
+
+        public bool[] FinnaAbilities(int CharacterID)
+        {
+            bool[] gogn = new bool[25];
+           if (OpenConnection() == true)
+            {
+                //query-in sem á að keyrast (má líka fyrir ofan if setninguna
+                fyrirspurn = "SELECT * FROM abilitis WHERE CharacterID ='" + CharacterID + "'";
+                //create command and assign the the query(fyrirspurn) and connection(tengingu)
+                nySQLskipun = new MySqlCommand(fyrirspurn, sqltenging);
+                sqllesari = nySQLskipun.ExecuteReader();
+                while (sqllesari.Read())
+                {
+                    for (int i = 0; i < 25; i++)
+                    {
+                        if (i != 0) { 
+                            if (Convert.ToInt32(sqllesari.GetValue(i).ToString()) == 1)
+                            { gogn[i] = true; }
+                            else
+                            { gogn[i] = false; }
+                        }
+                    }
+                }
+                sqllesari.Close();
+                CloseConnection();
+            }
+            /*
+            gogn[0] = true;
+            gogn[1] = false;
+            gogn[2] = false;
+            gogn[3] = false;
+            gogn[4] = true;
+            gogn[5] = true;
+            gogn[6] = false;
+            gogn[7] = false;
+            gogn[8] = true;
+            gogn[9] = true;
+            gogn[10] = false;
+            gogn[11] = true;
+            gogn[12] = true;
+            gogn[13] = false;
+            gogn[14] = true;
+            gogn[15] = false;
+            gogn[16] = true;
+            gogn[17] = false;
+            gogn[18] = false;
+            gogn[19] = true;
+            gogn[20] = true;
+            gogn[21] = false;
+            gogn[22] = true;
+            gogn[23] = false;
+            gogn[24] = true;
+            gogn[25] = false;
+            */
+
+            return gogn;
+        }
+
+
+        public void UppfaeraCharacter(int CharacterID, int[] stats, bool[] abilities)
+        {
+            int[] gogn = new int[25];
+            if (OpenConnection() == true)
+            {
+                /*query-in sem á að keyrast (má líka fyrir ofan if setninguna*/
+                fyrirspurn = "UPDATE stats " +
+                "SET level='" + stats[1] + "', " +
+                "XP='" + stats[2] + "', " +
+                "AC='" + stats[3] + "', " +
+                "MaxHP='" + stats[4] + "', " +
+                "HP='" + stats[5] + "', " +
+                "STR='" + stats[6] + "', " +
+                "DEX='" + stats[7] + "', " +
+                "CON='" + stats[8] + "', " +
+                "INTE='" + stats[9] + "', " +
+                "WIS='" + stats[10] + "', " +
+                "CHA='" + stats[11] + "', " +
+                "STR_MOD='" + stats[12] + "', " +
+                "DEX_MOD='" + stats[13] + "', " +
+                "CON_MOD='" + stats[14] + "', " +
+                "INT_MOD='" + stats[15] + "', " +
+                "WIS_MOD='" + stats[16] + "', " +
+                "CHA_MOD='" + stats[17] + "' " +
+                "WHERE CharacterId ='" + CharacterID + "'";
+                /*create command and assign the the query(fyrirspurn) and connection(tengingu)
+                 nySQLskipun = new MySqlCommand(fyrirspurn, sqltenging);
+                 /*ExecuteNonQuery: Used to execute a command that will not reutrn any data*/
+                nySQLskipun.ExecuteNonQuery();
+
+                for (int i = 0; i < 26; i++)
+                {
+                    if (abilities[i] == true)
+                    { gogn[i] = 1; }
+                    else
+                    { gogn[i] = 0; }
+                }
+
+               
+                fyrirspurn = "UPDATE abilities " +
+                "SET STR_ST='" +gogn[1] + "', " +
+                "Athletics='" + gogn[2] + "', " +
+                "DEX_ST='" + gogn[3] + "', " +
+                "Acrobatics='" + gogn[4] + "', " +
+                "SleightOfHand='" + gogn[5] + "', " +
+                "Stealth='" + gogn[6] + "', " +
+                "CON_ST='" + gogn[7] + "', " +
+                "INT_ST='" + gogn[8] + "', " +
+                "Arcana='" + gogn[9] + "', " +
+                "History='" + gogn[10] + "', " +
+                "Investigation='" + gogn[11] + "', " +
+                "Nature='" + gogn[12] + "', " +
+                "Religion='" + gogn[13] + "', " +
+                "WIS_ST='" + gogn[14] + "', " +
+                "Insight='" + gogn[15] + "', " +
+                "Animalhandling='" + gogn[16] + "', " +
+                "Medicine='" + gogn[17] + "', " +
+                "Perception='" + gogn[18] + "', " +
+                "Survival='" + gogn[19] + "', " +
+                "Cha_ST='" + gogn[20] + "', " +
+                "Deception='" + gogn[21] + "', " +
+                "Intimidation='" + gogn[22] + "', " +
+                "Preformance='" + gogn[23] + "', " +
+                "Persuasion='" + gogn[24] + "' " +
+                "WHERE CharacterId ='" + CharacterID + "'";
+                CloseConnection();
+            }
+        }
+
+    }
+}
+
